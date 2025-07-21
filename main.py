9ebc93af2b98684960dfde31de0f3213b9d85f6c
JakubPonulak <jakub.ponulak@gmail.com>
2025-07-21 11:20:39 -0700

    replaced single prompt with messages list

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-import sys
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
 def main():
-    if len(sys.argv) == 2:
-        prompt = sys.argv[1]
-    else:
-        print("Error, no prompt was given.")
+    load_dotenv()
+    
+    args = sys.argv[1:]
+
+    if not args:
+        print("Error: no prompt given")
+        print("Usage: uv run main.py 'prompt here'")
         sys.exit(1)
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=prompt)
-    print(response.text)
+    user_prompt = " ".join(args)
+    
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', 
+        contents=messages
+        )
+    
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
